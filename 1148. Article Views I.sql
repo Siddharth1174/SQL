d5ac252198865b19dac9dbf46e97f55d51ec6e9f
Siddharth R Sharan <142529706+Siddharth1174@users.noreply.github.com>
2024-10-21 21:42:33 +0530

    Create 1148. Article Views I.sql

---
+++ 1148. Article Views I.sql
@@ -0,0 +1,7 @@
+//Problem Link:
+https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
+//Solution:
+select distinct author_id as id 
+from Views 
+where author_id=viewer_id
+order by author_id;
